79025bab959db3e7a87420da97ae7b0ffff4a1ca
Anis Boubaker <anis@boubaker.ca>
2022-01-24 09:00:14 -0500

    Gr02-C3

---
+++ Gr02/C03/Exercice/main.c
@@ -0,0 +1,28 @@
+/*
+1. Complétez le programme suivant pour que la programme affiche 10 puis 3. 
+2. Modifiez votre programme, pour que les valeurs soient saisies par l'usager, 
+   puis que la plus petite valeur soit affichée en premier. 
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	/**  Ne pas modifier ces deux lignes **/
+	int val1 = 3; 
+	int val2 = 10; 
+	/**************************************/
+	
+
+
+
+
+
+	/* Ne pas modifier les lignes ci-dessous */
+	printf("val1 = %d, val2= %d", val1, val2);
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
