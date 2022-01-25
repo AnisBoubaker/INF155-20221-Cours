a525e14c3bfe658b1cc85f4813ce14d338f6502c
Anis Boubaker <anis@boubaker.ca>
2022-01-25 15:37:53 -0500

    Gr1 - C3

---
+++ Gr01/C03/Exercice2/main.c
@@ -0,0 +1,12 @@
+/*
+Exercice2: 
+
+Programme qui demande à l'usager de saisir 5 entiers. Ensuite vous lui affichez la
+moyenne des valeurs saisies. 
+
+- Le faire dans un premier temps sans utiliser les boucles avec 5 variables différentes 
+de saisie
+- Dans un 2e temps, éliminez les 5 variables pour en conserver juste une. 
+- Finalement convertir le programme en utilisant les boucles. 
+
+*/
