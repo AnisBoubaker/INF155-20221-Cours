24d5e456d0ed07bd1d1495cae9066aeb80a95b1b
Anis Boubaker <anis@boubaker.ca>
2022-01-25 13:46:12 -0500

    Gr1 - C3

---
+++ Gr01/C03/Exercice/main.c
@@ -0,0 +1,25 @@
+/*
+1. Compl`'etez le programme suivant pour que la programme affiche 10 puis 3.
+2. Modifiez votre programme, pour que les valeurs soient saisies par l'usager,
+   puis que la plus petite valeur soit affichée en premier.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	/**  Ne pas modifier ces deux lignes **/
+	int val1;
+	int val2;
+	/**************************************/
+	
+
+	/* Ne pas modifier les lignes ci-dessous */
+	printf("val1 = %d, val2= %d\n", val1, val2);
+	
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
