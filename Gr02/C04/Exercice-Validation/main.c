d0a5750b13624356c7cb9ec960d79183580bda77
Anis Boubaker <anis@boubaker.ca>
2022-01-31 08:35:25 -0500

    Gr2 - C4

---
+++ Gr02/C04/Exercice-Validation/main.c
@@ -0,0 +1,20 @@
+/*
+Exercice: Écrire un programme qui demande à l'usager de saisir une valeur entière 
+dans l'intervalle ]0..100]. 
+
+Si la valeur saisie par l'usager n'est pas dans le bon intervalle, lui redemander la saisie
+jusqu'à obtention d'une valeur satisfaisante. 
+
+Une fois qu'on a obtenu une valeur correcte, terminer le programme en affichant la valeur 
+saisie.
+
+Exemple: 
+
+Veuillez saisir une valeur dans ]0..100]: 250
+Veuillez saisir une valeur dans ]0..100]: 110
+Veuillez saisir une valeur dans ]0..100]: -20
+Veuillez saisir une valeur dans ]0..100]: 0
+Veuillez saisir une valeur dans ]0..100]: -100
+Veuillez saisir une valeur dans ]0..100]: 50
+Vous avez saisi: 50. 
+*/
