c7e493ccd70125255140a982c81edf86431fa477
Anis Boubaker <anis@boubaker.ca>
2022-02-01 15:18:25 -0500

    C04 - Gr01

--- Gr01/C04/Exercice_Factorielle/main.c
+++ Gr01/C04/Exercice_Factorielle/main.c
@@ -13,3 +13,27 @@ Saisir un entier < 15: 5
 Factorielle de 5 = 120
 
 */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int max;
+	int i; 
+	int resultat = 1; 
+
+	printf("Entrer un entier <15: ");
+	scanf("%d", &max);
+
+	for (i = 1; i <= max; i++)
+	{
+		resultat = resultat * i;
+	}
+
+	printf("La factorielle de %d est: %d\n", max, resultat);
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
