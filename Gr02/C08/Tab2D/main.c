e95ead2de200cb132dd2bea33bb91f00cc1d7938
Anis Boubaker <anis@boubaker.ca>
2022-03-07 11:38:07 -0500

    C08 gr02

--- Gr02/C08/Tab2D/main.c
+++ Gr02/C08/Tab2D/main.c
@@ -9,16 +9,19 @@ int main(void)
 {
 	int tab1d[10] = {10, 20, 30, 35, 45, 55}; //10, 20, 30, 35, 45, 55, 
 
+	//Déclaration d'un tableau à deux dimensions, et initialisation des 3 premières lignes
+	//Le reste des cases recevra la valeur 0. 
 	int tablo2d[10][5] = { {3, 6, 9}, {56, 89, 10}, {12, 25, 67, 90, 100} };
 
 	/*tablo2d[0][3] = 10;
 	tablo2d[2][4] = 50;
 	tablo2d[3][0] = 5;*/
 
+	//Parcours et affichage du contenu du tableau
 	for (int ligne = 0; ligne < 10; ligne++)
-	{ //ligne =1 
+	{  
 		for (int colonne = 0; colonne < 5; colonne++)
-		{ //colonne =1 
+		{  
 			printf("%d\t", tablo2d[ligne][colonne]);
 		}
 		printf("\n");
